0e80e4f1257d9351fa6f4082dec807c476b8bcaf
Antonio Mika <antoniomika@gmail.com>
2024-11-19 13:38:29 -0500

    Revert "Cleanup"
    This reverts commit 9265b45897ffe26d2f8a5bad5fff0f9b212f74f7.

--- pipe/metrics/metrics.go
+++ pipe/metrics/metrics.go
@@ -16,6 +16,21 @@ const (
 	SubCommand = "sub " + Topic + " -k"
 )
 
+// RegisterReconnectMetricRecorder registers a logger that forwards log records to a remote log drain and reconnects even if the initial connection fails.
+func RegisterReconnectMetricRecorder(ctx context.Context, logger *slog.Logger, info *pipe.SSHClientInfo, buffer int, timeout time.Duration) io.Writer {
+	reconnectMetricRecorder := pipe.NewReconnectReadWriteCloser(
+		ctx,
+		logger,
+		info,
+		ID,
+		PubCommand,
+		buffer,
+		timeout,
+	)
+
+	return reconnectMetricRecorder
+}
+
 // ReconnectReadMetrics reads metrics from a remote metric drain and reconnects even if the initial connection fails.
 func ReconnectReadMetrics(ctx context.Context, logger *slog.Logger, connectionInfo *pipe.SSHClientInfo, buffer int, timeout time.Duration) io.Reader {
 	reconnectMetricReader := pipe.NewReconnectReadWriteCloser(
